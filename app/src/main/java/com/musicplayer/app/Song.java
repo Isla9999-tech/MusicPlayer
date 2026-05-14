23e7557d75dff8f259cc3fc469cb831bdde393e2
Ubuntu <ubuntu@localhost.localdomain>
2026-05-14 20:57:34 +0800

    fix: Song implements Parcelable for Intent extras

--- app/src/main/java/com/musicplayer/app/Song.java
+++ app/src/main/java/com/musicplayer/app/Song.java
@@ -1,8 +1,10 @@
 package com.musicplayer.app;
 
 import android.net.Uri;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Song {
+public class Song implements Parcelable {
     private long id;
     private String title;
     private String artist;
@@ -15,6 +17,33 @@ public class Song {
         this.artist = artist;
         this.duration = duration;
         this.uri = uri;
+    }
+
+    protected Song(Parcel in) {
+        id = in.readLong();
+        title = in.readString();
+        artist = in.readString();
+        duration = in.readLong();
+        uri = in.readParcelable(Uri.class.getClassLoader());
+    }
+
+    public static final Creator<Song> CREATOR = new Creator<Song>() {
+        @Override
+        public Song createFromParcel(Parcel in) { return new Song(in); }
+        @Override
+        public Song[] newArray(int size) { return new Song[size]; }
+    };
+
+    @Override
+    public int describeContents() { return 0; }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeString(title);
+        dest.writeString(artist);
+        dest.writeLong(duration);
+        dest.writeParcelable(uri, flags);
     }
 
     public long getId() { return id; }
